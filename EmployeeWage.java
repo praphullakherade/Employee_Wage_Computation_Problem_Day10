f3c8819bbe8ef7389f8b44dd5d3b1a6fa45b5821
Praphulla Kherade <prafullkherade9@gmail.com>
2021-11-01 15:25:16 +0530

    Add part time employee and wage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -5,18 +5,21 @@ import java.util.Random;
 public class EmployeeWage {
 
     public static void main(String[] args) {
-        System.out.println("Welcome To Employee Wage Compuation Program");
-        Random num = new Random ();
-        int CheckAvail= num.nextInt(2);
-        System.out.println(" CheckAvail is " + CheckAvail);
-        int WagePerHour = 20;
-        int FulldayHour = 8;
-        if (CheckAvail == 1)
-        {
-            int DailyEmpWage =  WagePerHour *FulldayHour ;
-            System.out.println("Employee is Present and Employee Daily Wage is " + DailyEmpWage);
-        }
+        System.out.println("Welcome To Employee Wage Computation Program");
+        int IS_FULL_TIME = 1;
+        int IS_PART_TIME = 2;
+        int WAGE_PER_HR = 20;
+        int EMP_HRS;
+        int TotalWageInDay;
+        Random num = new Random();
+        int randomCheck = num.nextInt(3);
+        if (IS_FULL_TIME == randomCheck)
+            EMP_HRS = 8;
+        else if (IS_PART_TIME == randomCheck)
+            EMP_HRS = 4;
         else
-            System.out.println("Employee is Absent");
+            EMP_HRS =0;
+        TotalWageInDay = WAGE_PER_HR * EMP_HRS;
+        System.out.println("Total wage in Day : "+TotalWageInDay);
     }
 }
