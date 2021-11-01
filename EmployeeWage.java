57a4ab98f504cdcb9e521b09b67b2d9c76274400
Praphulla Kherade <prafullkherade9@gmail.com>
2021-11-01 15:20:48 +0530

    Calculate Daily Employee Wage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,13 +1,21 @@
 package com.employeewage;
 
+import java.util.Random;
+
 public class EmployeeWage {
 
     public static void main(String[] args) {
         System.out.println("Welcome To Employee Wage Compuation Program");
-        int IS_FULL_TIME = 1;
-        double empCheck = Math.floor(Math.random() * 10) % 2;
-        if (empCheck == IS_FULL_TIME)
-            System.out.println("Employee is Present");
+        Random num = new Random ();
+        int CheckAvail= num.nextInt(2);
+        System.out.println(" CheckAvail is " + CheckAvail);
+        int WagePerHour = 20;
+        int FulldayHour = 8;
+        if (CheckAvail == 1)
+        {
+            int DailyEmpWage =  WagePerHour *FulldayHour ;
+            System.out.println("Employee is Present and Employee Daily Wage is " + DailyEmpWage);
+        }
         else
             System.out.println("Employee is Absent");
     }
